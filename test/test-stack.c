dccbae5d386d3cda806ae25c0d503019b2db1ef6
Sebastien Goubeau <sebastien.goubeau@epita.fr>
2020-05-25 01:08:03 +0200

    test-suite: Update of Stack test-suite section

--- test/test-stack.c
+++ test/test-stack.c
@@ -28,37 +28,15 @@ struct test_rate test_stack()
   node2.val = (void*)&two;
   node3.val = (void*)&three;
 
-//  printf("Inisialising stack...\n");
   struct stack_node stack;
   stack_initStack(&stack);
 
-//  printf("Filling stack...\n");
   stack_push(&stack, &node0);
   stack_push(&stack, &node1);
   stack_push(&stack, &node2);
   stack_push(&stack, &node3);
 
-/*
-  printf("Is stack empty (should be no) : ");
-  if (stack_isStackEmpty(&stack))
-    printf("yes\n");
-  else
-    printf("no\n");
-*/
   test_assert(&res, "Stack empty after fill", !stack_isStackEmpty(&stack));
-
-/*
-  printf("Poping the stack...\n");
-  printf("Node3 poped : value = %d (should be 3)\n", *(int*)(stack_pop(&stack)->val));
-  printf("Node2 poped : value = %d (should be 2)\n", *(int*)(stack_pop(&stack)->val));
-  printf("Node1 poped : value = %d (should be 1)\n", *(int*)(stack_pop(&stack)->val));
-  printf("Node0 poped : value = %d (should be 0)\n", *(int*)(stack_pop(&stack)->val));
-  printf("Poped empty stack (should be NULL) : ");
-  if (stack_pop(&stack) == NULL)
-    printf("NULL\n");
-  else
-    printf("NOT NULL\n");
-*/
   test_assert(&res, "Poping 1/4", *(int*)(stack_pop(&stack)->val) == 3);
   test_assert(&res, "Poping 2/4", *(int*)(stack_pop(&stack)->val) == 2);
   test_assert(&res, "Poping 3/4", *(int*)(stack_pop(&stack)->val) == 1);
